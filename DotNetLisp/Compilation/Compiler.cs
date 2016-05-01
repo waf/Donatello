92f029457b065a0c16d8ce6854fef024e44eeded
Will Fuqua <wafuqua@gmail.com>
2016-05-01 12:02:25 +0700

    add result type and make compiler code use it

---
+++ DotNetLisp/Compilation/Compiler.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using static Microsoft.CodeAnalysis.CSharp.SyntaxFactory;
+using Microsoft.CodeAnalysis.Emit;
+using System.IO;
+using Newtonsoft.Json;
+using System.Reflection;
+using DotNetLisp.Util;
+
+namespace DotNetLisp.Compilation
+{
+    public static class Compiler
+    {
+        /// <summary>
+        /// Compile the roslyn AST
+        /// </summary>
+        /// <returns>Either the compiled bytes of the program, or a list of error messages</returns>
+        public static Result<byte[], string[]> Compile(ExpressionSyntax programExpression)
+        {
+            // make a Program class that has a "Run" method, and embed our program expression inside it.
+            var program = CompilationUnit()
+                .AddMembers(NamespaceDeclaration(IdentifierName("Repl"))
+                    .AddMembers(ClassDeclaration("Program")
+                        .AddMembers(MethodDeclaration(ParseTypeName("System.Object"), "Run")
+                           .AddModifiers(Token(SyntaxKind.PublicKeyword), Token(SyntaxKind.StaticKeyword))
+                           .WithBody(Block(ReturnStatement(programExpression))))));
+
+            MetadataReference[] references = {
+                MetadataReference.CreateFromFile(typeof(object).Assembly.Location),
+                MetadataReference.CreateFromFile(typeof(Enumerable).Assembly.Location)
+            };
+
+            CSharpCompilation compilation = CSharpCompilation.Create(
+                Path.GetRandomFileName(),
+                syntaxTrees: new[] { CSharpSyntaxTree.Create(program) },
+                references: references,
+                options: new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary));
+
+            using (var ms = new MemoryStream())
+            {
+                EmitResult emmitted = compilation.Emit(ms);
+
+                if (!emmitted.Success)
+                {
+                    // create error messages
+                    return emmitted.Diagnostics
+                        .Where(diagnostic =>
+                            diagnostic.IsWarningAsError ||
+                            diagnostic.Severity == DiagnosticSeverity.Error)
+                        .Select(diagnostic => $"{diagnostic.Id}: {diagnostic.GetMessage()}")
+                        .ToArray();
+                }
+
+                // load the program and run it
+                ms.Seek(0, SeekOrigin.Begin);
+                return ms.ToArray();
+            }
+        }
+    }
+}
--- DotNetLisp/Program.cs
+++ DotNetLisp/Program.cs
@@ -1,23 +1,17 @@
 ﻿using Antlr4.Runtime;
+using Antlr4.Runtime.Tree;
 using DotNetLisp.Antlr.Generated;
+using DotNetLisp.Compilation;
+using DotNetLisp.Parser;
+using DotNetLisp.Util;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Newtonsoft.Json;
 using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Linq.Expressions;
-using System.Text;
-using System.Threading.Tasks;
-using DotNetLisp.Parser;
-using Antlr4.Runtime.Misc;
-using Antlr4.Runtime.Sharpen;
-using Antlr4.Runtime.Tree;
-using Microsoft.CodeAnalysis.CSharp.Syntax;
-using Microsoft.CodeAnalysis;
-using Microsoft.CodeAnalysis.CSharp;
-using Microsoft.CodeAnalysis.Emit;
-using static Microsoft.CodeAnalysis.CSharp.SyntaxFactory;
 using System.Reflection;
+using System.Threading.Tasks;
+using static Microsoft.CodeAnalysis.CSharp.SyntaxFactory;
 
 namespace DotNetLisp
 {
@@ -42,9 +36,9 @@ namespace DotNetLisp
 
         private static void ReadEvalPrintLoop()
         {
-            // there's a slight delay when we load up roslyn and run the program for the first time. Do an
+            // there's a slight delay when we load up roslyn and run a program for the first time. Do an
             // initial run to warm things up, so the user doesn't experience the delay for the first evaluation.
-            Task.Run(() => CompileAndRun(LiteralExpression(SyntaxKind.StringLiteralExpression)));
+            Task.Run(() => Compiler.Compile(LiteralExpression(SyntaxKind.StringLiteralExpression)));
 
             while (true)
             {
@@ -59,8 +53,12 @@ namespace DotNetLisp
                 string output = "";
                 try
                 {
-                    var program = Evaluate(input);
-                    output = CompileAndRun(program);
+                    var ast = Evaluate(input);
+                    var assembly = Compiler.Compile(ast);
+                    // either run the compiled output or handle the errors
+                    output = assembly.Match(
+                        Ok: bytes => FormatProgramOutput(DynamicInvoke(bytes)),
+                        Error: errors => string.Join(Environment.NewLine, errors));
                 }
                 catch (Exception e)
                 {
@@ -70,6 +68,19 @@ namespace DotNetLisp
                 // print
                 Console.WriteLine(output);
             } // loop!
+        }
+
+        private static string FormatProgramOutput(object output)
+        {
+            return JsonConvert.SerializeObject(output, Formatting.Indented);
+        }
+
+        private static object DynamicInvoke(byte[] bytes)
+        {
+            Assembly assembly = Assembly.Load(bytes);
+            Type type = assembly.GetType("Repl.Program");
+            var result = type.InvokeMember("Run", BindingFlags.InvokeMethod | BindingFlags.Public | BindingFlags.Static, null, null, null);
+            return result;
         }
 
         /// <summary>
@@ -89,54 +100,6 @@ namespace DotNetLisp
                 var file = parser.file();
 
                 return visitor.Visit(file);
-            }
-        }
-
-        /// <summary>
-        /// Compile and run the roslyn AST
-        /// </summary>
-        /// <returns>The output of the program</returns>
-        private static string CompileAndRun(ExpressionSyntax programExpression)
-        {
-            // make a Program class that has a "Run" method, and embed our program expression inside it.
-            var program = CompilationUnit()
-                .AddMembers(NamespaceDeclaration(IdentifierName("Repl"))
-                    .AddMembers(ClassDeclaration("Program")
-                        .AddMembers(MethodDeclaration(ParseTypeName("System.Object"), "Run")
-                           .AddModifiers(Token(SyntaxKind.PublicKeyword), Token(SyntaxKind.StaticKeyword))
-                           .WithBody(Block(ReturnStatement(programExpression))))));
-
-            MetadataReference[] references = {
-                MetadataReference.CreateFromFile(typeof(object).Assembly.Location),
-                MetadataReference.CreateFromFile(typeof(Enumerable).Assembly.Location)
-            };
-
-            CSharpCompilation compilation = CSharpCompilation.Create(
-                Path.GetRandomFileName(),
-                syntaxTrees: new[] { CSharpSyntaxTree.Create(program) },
-                references: references,
-                options: new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary));
-
-            using (var ms = new MemoryStream())
-            {
-                EmitResult emmitted = compilation.Emit(ms);
-
-                if (!emmitted.Success)
-                {
-                    // create error messages
-                    return string.Join(Environment.NewLine, emmitted.Diagnostics
-                        .Where(diagnostic =>
-                            diagnostic.IsWarningAsError ||
-                            diagnostic.Severity == DiagnosticSeverity.Error)
-                        .Select(diagnostic => $"{diagnostic.Id}: {diagnostic.GetMessage()}"));
-                }
-
-                // load the program and run it
-                ms.Seek(0, SeekOrigin.Begin);
-                Assembly assembly = Assembly.Load(ms.ToArray());
-                Type type = assembly.GetType("Repl.Program");
-                var result = type.InvokeMember("Run", BindingFlags.InvokeMethod | BindingFlags.Public | BindingFlags.Static, null, null, null);
-                return JsonConvert.SerializeObject(result, Formatting.Indented);
             }
         }
     }
