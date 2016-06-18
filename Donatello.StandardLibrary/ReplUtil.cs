ce4aeb9eedf372289dfcd660b4084d3fa05f8111
Will Fuqua <wafuqua@gmail.com>
2016-06-19 00:29:38 +0700

    make the repl-reported types prettier

--- Donatello.StandardLibrary/ReplUtil.cs
+++ Donatello.StandardLibrary/ReplUtil.cs
@@ -1,5 +1,7 @@
 ﻿using Newtonsoft.Json;
 using System;
+using System.CodeDom;
+using System.CodeDom.Compiler;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -22,13 +24,50 @@ namespace Donatello.StandardLibrary
 
         public static void Print(object obj)
         {
-            if(obj == null)
+            if (obj == null)
             {
                 Console.WriteLine("null");
             }
             string output = JsonConvert.SerializeObject(obj, Formatting.Indented);
-            string type = obj.GetType().Name;
+            string type = PrettyPrintTypeName(obj.GetType());
             Console.WriteLine($"{output} :{type}");
         }
+
+        private static string PrettyPrintTypeName(Type type)
+        {
+            string name;
+            if(Aliases.TryGetValue(type, out name))
+            {
+                return name;
+            }
+
+            string typeName = type.FullName.Replace(type.Namespace + ".", "");//Removing the namespace
+            using (var provider = CodeDomProvider.CreateProvider("CSharp"))
+            {
+                var reference = new CodeTypeReference(typeName);
+                return provider.GetTypeOutput(reference);
+            }
+        }
+
+        private static readonly Dictionary<Type, string> Aliases =
+            new Dictionary<Type, string>()
+        {
+            { typeof(byte), "byte" },
+            { typeof(sbyte), "sbyte" },
+            { typeof(short), "short" },
+            { typeof(ushort), "ushort" },
+            { typeof(int), "int" },
+            { typeof(uint), "uint" },
+            { typeof(long), "long" },
+            { typeof(ulong), "ulong" },
+            { typeof(float), "float" },
+            { typeof(double), "double" },
+            { typeof(decimal), "decimal" },
+            { typeof(object), "object" },
+            { typeof(bool), "bool" },
+            { typeof(char), "char" },
+            { typeof(string), "string" },
+            { typeof(void), "void" }
+        };
     }
 }
